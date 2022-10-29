14d2e3ab2b0ff6900a8f54100446a65b7f22b536
Peter Shulha <forest23557@gmail.com>
2022-10-29 12:10:44 +0300

    I have changed the structure of the files

--- src/main/java/com/shulha/Main.java
+++ src/main/java/com/shulha/Main.java
@@ -1,83 +1,27 @@
 package com.shulha;
-import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 //      1
-        System.out.println("Task 1");
-
-        Random rand = new Random();
-        int upperBound = 100;
-
-        int sideA = rand.nextInt(upperBound);
-        int sideB = rand.nextInt(upperBound);
-        int sideC = rand.nextInt(upperBound);
-
-        if ((sideA < sideB + sideC) && (sideB < sideA + sideC) && (sideC < sideA + sideB)) {
-            float p = (float) (sideA + sideB + sideC) / 2;
-            int s = (int) Math.round(Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC)));
-
-            System.out.println("The area of the triangle with ");
-            System.out.println("the side A: " + sideA + ", ");
-            System.out.println("the side B: " + sideB);
-            System.out.println("and the side C: " + sideC);
-            System.out.println("equals - " + s);
-            System.out.println();
-        } else {
-            System.out.println("We have wrong sides of the triangle!");
-            System.out.println();
-        }
+        String name = "Peter";
+        String surname = "Shulha";
+        System.out.println(name + " " + surname);
 
 //      2
-        System.out.println("Task 2");
-
-        int number1 = rand.nextInt(upperBound) - 50;
-        int number2 = rand.nextInt(upperBound) - 50;
-        int number3 = rand.nextInt(upperBound) - 50;
-
-        System.out.println("Numbers 1: " + number1 + ", 2: " + number2 + ", 3: " + number3);
-
-        int lowestNum = Math.abs(number1) < Math.abs(number2) ? number1 : number2;
-        lowestNum = Math.abs(lowestNum) < Math.abs(number3) ? lowestNum : number3;
-
-        System.out.println("The number module with the lowest value: " + Math.abs(lowestNum));
-        System.out.println();
+        int y = 5;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("The step " + i + ", the value " + y);
+            y += 2;
+        }
 
 //      3
-        System.out.println("Task 3");
-
-        upperBound = 1000;
-        int randomNumber = rand.nextInt(upperBound);
-
-        if (randomNumber % 2 != 1 ) {
-            System.out.println("The random number " + randomNumber + " is paired");
-        } else {
-            System.out.println("The random number " + randomNumber + " isn't paired");
-        }
-
-        System.out.println();
-
-//      Additional task
-        System.out.println("Additional task");
-
-        upperBound = 255;
-        double numberForChanging = rand.nextInt(upperBound);
-        String binaryNumber = "";
-        System.out.println("The decimal number: " + Math.round(numberForChanging));
-
-        while (numberForChanging != 0) {
-            if (numberForChanging % 2 == 1) {
-                binaryNumber += 1;
-            } else {
-                binaryNumber += 0;
+        for (int i = 0; i <= 10; i++) {
+            if (i == 3) {
+                continue;
+            } else if (i == 6) {
+                break;
             }
-            numberForChanging = Math.floor(numberForChanging / 2);
+            System.out.println("The step " + i);
         }
-
-        StringBuilder sb = new StringBuilder(binaryNumber);
-        sb.reverse();
-        binaryNumber = sb.toString();
-        System.out.println("to the binary number equals: " + binaryNumber);
-        System.out.println();
     }
 }
