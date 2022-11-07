86a5b101990021664f97186ea4db2d37d16d2114
Peter Shulha <forest23557@gmail.com>
2022-11-07 19:47:33 +0200

    Hw6 - I have removed all classes except class Main and I have started working

--- src/main/java/com/shulha/Main.java
+++ src/main/java/com/shulha/Main.java
@@ -2,26 +2,6 @@ package com.shulha;
 
 public class Main {
     public static void main(String[] args) {
-//      1
-        String name = "Peter";
-        String surname = "Shulha";
-        System.out.println(name + " " + surname);
 
-//      2
-        int y = 5;
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("The step " + i + ", the value " + y);
-            y += 2;
-        }
-
-//      3
-        for (int i = 0; i <= 10; i++) {
-            if (i == 3) {
-                continue;
-            } else if (i == 6) {
-                break;
-            }
-            System.out.println("The step " + i);
-        }
     }
 }
