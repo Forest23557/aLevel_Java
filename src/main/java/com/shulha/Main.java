e0a99fff3895c98a8cb61f074de0f2c31c35e679
Peter Shulha <forest23557@gmail.com>
2022-12-11 16:50:46 +0200

    hw13 - I have made some changes in bubbleSort method. BinarySearch method has been created and tested.

--- src/main/java/com/shulha/Main.java
+++ src/main/java/com/shulha/Main.java
@@ -7,23 +7,35 @@ import com.shulha.util.RandomGenerator;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         final CarService carService = CarService.getInstance();
 
         final Car car = carService.createCar(CarTypes.CAR);
+        final String id = car.getId();
         carService.createCar(9, CarTypes.CAR);
 
 //        int[] array = {10, 4, 48, 32, 56, 60};
 //        System.out.println(Arrays.toString(array));
 //        AlgorithmUtil.bubbleSort(array);
 //        System.out.println(Arrays.toString(array));
+//
+//        System.out.println(AlgorithmUtil.binarySearch(array, 0, array.length - 1, 60));
 
         carService.printAll();
-        AlgorithmUtil.bubbleSort(carService.getAll());
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        carService.printAll();
+        final Car[] sortedCarArray = AlgorithmUtil.bubbleSort(carService.getAll());
+        System.out.println("~".repeat(20));
+        for (int i = 0; i < sortedCarArray.length; i++) {
+            carService.print(sortedCarArray[i]);
+        }
+
+        final Car car1 = AlgorithmUtil.binarySearch(sortedCarArray,
+                0, carService.getAll().length - 1, id);
+
+        System.out.println("~".repeat(20));
+        System.out.println(car1);
 
 //        carService.printManufacturerAndCount(car);
 //        carService.printManufacturerAndCount(null);
--- src/main/java/com/shulha/util/AlgorithmUtil.java
+++ src/main/java/com/shulha/util/AlgorithmUtil.java
@@ -8,14 +8,18 @@ import java.util.Optional;
 public class AlgorithmUtil {
     private static final CarService CAR_SERVICE = CarService.getInstance();
 
-    public static void bubbleSort(final Car[] carArray) {
+    public static Car[] bubbleSort(final Car[] carArray) {
         if (Optional.ofNullable(carArray).isPresent()) {
             int length = carArray.length;
+            final Car[] sortedCarArray = carArray.clone();
 
             for (int i = length - 1; i > 0; i--) {
                 for (int j = 0; j < i; j++) {
-                    if (CAR_SERVICE.compareCar(carArray[j], carArray[j + 1]) > 0) {
-                        CAR_SERVICE.replaceCarsFromRepository(j, j + 1);
+                    if (sortedCarArray[j].getId().compareTo(sortedCarArray[j + 1].getId()) > 0) {
+//                        CAR_SERVICE.replaceCarsFromRepository(j, j + 1);
+                        Car temp = sortedCarArray[j];
+                        sortedCarArray[j] = sortedCarArray[j + 1];
+                        sortedCarArray[j + 1] = temp;
                     }
 //                    if (carArray[j] > carArray[j + 1]) {
 //                        int temp = carArray[j];
@@ -24,8 +28,35 @@ public class AlgorithmUtil {
 //                    }
                 }
             }
+
+            return sortedCarArray;
         }
+
+        return null;
     }
 
+    public static Car binarySearch(final Car[] carArray, final int start, final int end, final String id) {
+        Optional<Car[]> safeCarArray = Optional.ofNullable(carArray);
 
+        if (safeCarArray.isPresent()) {
+            if (end >= start) {
+                int middleElement = (end + start) / 2;
+
+                boolean answer = carArray[middleElement].getId().equals(id);
+
+                if (answer) {
+                    return carArray[middleElement];
+                }
+
+                if (carArray[middleElement].getId().compareTo(id) > 0) {
+                    return binarySearch(carArray, start, middleElement - 1, id);
+                }
+                return binarySearch(carArray, middleElement + 1, end, id);
+            }
+
+            return null;
+        }
+
+        return null;
+    }
 }
